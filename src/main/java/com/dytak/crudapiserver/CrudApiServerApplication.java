984b44929833253dac094220ecc06b408eeef207
dytakk <82704585+dytakk@users.noreply.github.com>
2024-10-29 22:59:29 +0900

    init commit

---
+++ src/main/java/com/dytak/crudapiserver/CrudApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.dytak.crudapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dytak/crudapiserver/CrudApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dytak.crudapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
