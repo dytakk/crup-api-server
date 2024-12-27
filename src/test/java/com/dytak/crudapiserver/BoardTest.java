9e02d2eba2b8206c6a494e331d36e16d7bc5197a
dytakk <82704585+dytakk@users.noreply.github.com>
2024-12-27 10:19:55 +0900

    test: 게시글 리스트 및 게시글 읽기 테스트

--- src/test/java/com/dytak/crudapiserver/BoardTest.java
+++ src/test/java/com/dytak/crudapiserver/BoardTest.java
@@ -1,11 +1,16 @@
 package com.dytak.crudapiserver;
 
 import com.dytak.crudapiserver.board.BoardRepository;
+import com.dytak.crudapiserver.board.BoardService;
 import com.dytak.crudapiserver.entity.Board;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,17 +20,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BoardTest {
 
    private final BoardRepository boardRepository;
+   private final BoardService boardService;
 
    @Autowired
-   public BoardTest(BoardRepository boardRepository){
+   public BoardTest(BoardRepository boardRepository,BoardService boardService){
        this.boardRepository = boardRepository;
-
+       this.boardService=boardService;
    }
 
 
     @Test
+    @DisplayName("Board 게시글 리스트 페이징을 통한 조회 테스트")
+    public void boardList(){
+
+        //Given (Pageable의 사이즈는 임의값 5로 지정);
+        //When
+        Pageable pageable = PageRequest.of(0,5);
+        Page<Board> board = boardRepository.findAll(pageable);
+
+        //Then
+        assertThat(board.getTotalPages()).isGreaterThan(0);
+        assertThat(board.getTotalElements()).isGreaterThanOrEqualTo(1);
+        assertThat(board.getContent().get(0).getTitle()).isNotNull();
+
+    }
+
+
+    @Test
     @DisplayName("Board 게시글 읽기 테스트")
-    public void test(){
+    public void boardDetail(){
 
        //Given (테스트용으로 만든 게시글의 ID는 1)
         Long boardId=1L;
