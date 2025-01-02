36a18f21116aa949583a8d091ce1756fd8baf078
dytakk <82704585+dytakk@users.noreply.github.com>
2025-01-02 15:55:53 +0900

    add: Member테이블 및 테스트값 Insert 구문

--- docker/initdb.d/init.sql
+++ docker/initdb.d/init.sql
@@ -6,7 +6,7 @@ CREATE DATABASE `crud_api_server`;
 
 
 CREATE TABLE `crud_api_server`.`board` (
-                         `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+                         `id` bigint(20) NOT NULL AUTO_INCREMENT,
                          `title` varchar(100) NOT NULL,
                          `content` text NOT NULL,
                          `writer_id` int(8) NOT NULL,
@@ -16,5 +16,17 @@ CREATE TABLE `crud_api_server`.`board` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-
 INSERT INTO crud_api_server.board (title, content, writer_id) VALUES('title_test', 'content_test', 1);
+
+CREATE TABLE `member` (
+                          `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                          `username` varchar(50) NOT NULL,
+                          `password` varchar(100) DEFAULT NULL,
+                          `name` varchar(12) DEFAULT NULL,
+                          `grade` char(1) DEFAULT NULL,
+                          `mail_address` varchar(100) DEFAULT NULL,
+                          PRIMARY KEY (`id`),
+                          UNIQUE KEY `member_UN` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO crud_api_server.member (username, name) VALUES('test_id', '테스트이름');
