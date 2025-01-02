6e299dd97563693800a2c79a2a387435022ac546
dytakk <82704585+dytakk@users.noreply.github.com>
2025-01-02 15:52:59 +0900

    refactor: 불필요한 코드 및 개행 삭제

--- src/main/java/com/dytak/crudapiserver/board/BoardRepository.java
+++ src/main/java/com/dytak/crudapiserver/board/BoardRepository.java
@@ -1,6 +1,5 @@
 package com.dytak.crudapiserver.board;
 
-import com.dytak.crudapiserver.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends CrudRepository<Board,Long> {
-
 
     Page<Board> findAll(Pageable pageable);
 
