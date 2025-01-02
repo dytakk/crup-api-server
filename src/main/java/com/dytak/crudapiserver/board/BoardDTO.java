d4ce6441edb13417f70729e71259d0da0e5abd73
dytakk <82704585+dytakk@users.noreply.github.com>
2025-01-02 19:55:35 +0900

    fix: Request시 Body의 Snake Case를 자동으로 Camel Case에 맵핑되도록 수정

--- src/main/java/com/dytak/crudapiserver/board/BoardDTO.java
+++ src/main/java/com/dytak/crudapiserver/board/BoardDTO.java
@@ -1,6 +1,8 @@
 package com.dytak.crudapiserver.board;
 
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,14 +10,15 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BoardDTO {
 
     public Long id;
     public String title;
     public String content;
-    public Long writer_id;
+    public Long writerId;
     public Board toEntity(){
-        return Board.builder().id(id).content(content).title(title).writer_id(writer_id).build();
+        return Board.builder().id(id).content(content).title(title).writer_id(writerId).build();
     }
 
 }
