b6dc67f349071ddbc5a793ed4a28d40620894003
dytakk <82704585+dytakk@users.noreply.github.com>
2025-01-10 17:28:52 +0900

    fix: comment(댓글)DDL 추가 및 member ddl 삭제

--- docker/initdb.d/init.sql
+++ docker/initdb.d/init.sql
@@ -5,28 +5,25 @@ CREATE DATABASE `crud_api_server`;
 
 
 
-CREATE TABLE `crud_api_server`.`board` (
+CREATE TABLE `board` (
                          `id` bigint(20) NOT NULL AUTO_INCREMENT,
                          `title` varchar(100) NOT NULL,
                          `content` text NOT NULL,
-                         `writer_id` int(8) NOT NULL,
-                         `crt_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-                         `mod_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                         `writer_id` bigint(20) NOT NULL,
+                         `crt_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                         `mod_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-INSERT INTO crud_api_server.board (title, content, writer_id) VALUES('title_test', 'content_test', 1);
-
-CREATE TABLE `member` (
-                          `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                          `username` varchar(50) NOT NULL,
-                          `password` varchar(100) DEFAULT NULL,
-                          `name` varchar(12) DEFAULT NULL,
-                          `grade` char(1) DEFAULT NULL,
-                          `mail_address` varchar(100) DEFAULT NULL,
-                          PRIMARY KEY (`id`),
-                          UNIQUE KEY `member_UN` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO crud_api_server.member (username, name) VALUES('test_id', '테스트이름');
+CREATE TABLE `comment` (
+                           `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+                           `writer_id` mediumint(9) NOT NULL,
+                           `content` text DEFAULT NULL,
+                           `crt_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                           `mod_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+                           `board_id` bigint(20) NOT NULL,
+                           PRIMARY KEY (`id`),
+                           KEY `comment_FK` (`board_id`),
+                           CONSTRAINT `comment_FK` FOREIGN KEY (`board_id`) REFERENCES `board` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
