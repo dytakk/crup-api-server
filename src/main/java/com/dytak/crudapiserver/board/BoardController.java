56314b4d8b2a984b233665583922306c64b14025
dytakk <82704585+dytakk@users.noreply.github.com>
2025-02-04 19:37:39 +0900

    refactor: 공통응답DTO 추가

--- src/main/java/com/dytak/crudapiserver/board/BoardController.java
+++ src/main/java/com/dytak/crudapiserver/board/BoardController.java
@@ -1,10 +1,14 @@
 package com.dytak.crudapiserver.board;
 
+import com.dytak.crudapiserver.common.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
@@ -14,7 +18,7 @@ public class BoardController {
 
     @Autowired
     BoardController(BoardService boardService){
-        this.boardService=boardService;
+        this.boardService = boardService;
     }
 
     @PostMapping
@@ -32,13 +36,16 @@ public class BoardController {
         return ResponseEntity.ok().body(boardService.deleteBoard(id));
     }
 
+
     @GetMapping
     public ResponseEntity<?> getBoard(Pageable pageable){
-        return ResponseEntity.ok().body(boardService.getBoardList(pageable));
+        return ResponseEntity.ok().body(ResponseDTO.builder().code(200).msg("OK").data(Optional.of(boardService.getBoardList(pageable))).build());
     }
+
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getBoardById(@PathVariable Long id){
-        return ResponseEntity.ok().body(boardService.findBoardDetailById(id));
+        return ResponseEntity.ok().body(ResponseDTO.builder().code(200).msg("OK").data(Optional.ofNullable(boardService.findBoardDetailById(id))).build());
     }
 
 }
