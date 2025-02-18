b2e3c04f7abe9ee9814b87f38c980c5664c933f2
dytakk <82704585+dytakk@users.noreply.github.com>
2025-02-18 14:46:17 +0900

    refactor: 공통응답DTO 사용

--- src/main/java/com/dytak/crudapiserver/board/BoardController.java
+++ src/main/java/com/dytak/crudapiserver/board/BoardController.java
@@ -1,51 +1,63 @@
 package com.dytak.crudapiserver.board;
 
+import com.dytak.crudapiserver.common.ResCodeMsg;
 import com.dytak.crudapiserver.common.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
-    private final BoardService boardService;
+  private final BoardService boardService;
 
-    @Autowired
-    BoardController(BoardService boardService){
-        this.boardService = boardService;
-    }
+  @Autowired
+  BoardController(BoardService boardService) {
+    this.boardService = boardService;
+  }
 
-    @PostMapping
-    public ResponseEntity<?> saveBoard(@RequestBody BoardSaveDTO dto){
-        return ResponseEntity.ok().body(boardService.saveBoard(dto));
-    }
+  @PostMapping
+  public ResponseEntity<?> saveBoard(@RequestBody BoardSaveDTO dto) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(boardService.saveBoard(dto)).build());
+  }
 
-    @PutMapping
-    public ResponseEntity<?> modifyBoard(@RequestBody BoardSaveDTO dto){
-        return ResponseEntity.ok().body(boardService.modifyBoard(dto));
-    }
+  @PutMapping
+  public ResponseEntity<?> modifyBoard(@RequestBody BoardSaveDTO dto) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(boardService.modifyBoard(dto)).build());
+  }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteBoard(@PathVariable Long id){
-        return ResponseEntity.ok().body(boardService.deleteBoard(id));
-    }
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteBoard(@PathVariable Long id) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(boardService.deleteBoard(id)).build());
+  }
 
+  @GetMapping
+  public ResponseEntity<?> getBoard(Pageable pageable) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(boardService.getBoardList(pageable)).build());
+  }
 
-    @GetMapping
-    public ResponseEntity<?> getBoard(Pageable pageable){
-        return ResponseEntity.ok().body(ResponseDTO.builder().code(200).msg("OK").data(Optional.of(boardService.getBoardList(pageable))).build());
-    }
-
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getBoardById(@PathVariable Long id){
-        return ResponseEntity.ok().body(ResponseDTO.builder().code(200).msg("OK").data(Optional.ofNullable(boardService.findBoardDetailById(id))).build());
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getBoardById(@PathVariable Long id) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(boardService.findBoardDetailById(id)).build());
+  }
 
 }
--- src/main/java/com/dytak/crudapiserver/comment/CommentController.java
+++ src/main/java/com/dytak/crudapiserver/comment/CommentController.java
@@ -1,40 +1,56 @@
 package com.dytak.crudapiserver.comment;
 
 
+import com.dytak.crudapiserver.common.ResCodeMsg;
+import com.dytak.crudapiserver.common.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/comment")
 public class CommentController {
 
-    private final CommentService commentService;
+  private final CommentService commentService;
 
-    @Autowired
-    CommentController(CommentService commentService){
-        this.commentService=commentService;
-    }
+  @Autowired
+  CommentController(CommentService commentService) {
+    this.commentService = commentService;
+  }
 
-    @PostMapping
-    public ResponseEntity<?> saveComment(@RequestBody CommentSaveDTO dto){
-        return ResponseEntity.ok().body(commentService.saveComment(dto));
-    }
+  @PostMapping
+  public ResponseEntity<?> saveComment(@RequestBody CommentSaveDTO dto) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(commentService.saveComment(dto)).build());
+  }
 
-    @PutMapping
-    public ResponseEntity<?> modifyComment(@RequestBody CommentSaveDTO dto){
-        return ResponseEntity.ok().body(commentService.modifyComment(dto));
-    }
+  @PutMapping
+  public ResponseEntity<?> modifyComment(@RequestBody CommentSaveDTO dto) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(commentService.modifyComment(dto)).build());
+  }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteComment(@PathVariable Long id){
-        return ResponseEntity.ok().body(commentService.deleteComment(id));
-    }
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteComment(@PathVariable Long id) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(commentService.deleteComment(id)).build());
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getCommentById(@PathVariable Long id){
-        return ResponseEntity.ok().body(commentService.findCommentDetailById(id));
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getCommentById(@PathVariable Long id) {
+    return ResponseEntity.ok().body(
+        ResponseDTO.builder().code(ResCodeMsg.OK.getCode()).msg(ResCodeMsg.OK)
+            .data(commentService.findCommentDetailById(id)).build());
+  }
 
 }
